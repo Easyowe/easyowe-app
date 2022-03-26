8a19fe6fdd0fa186ecbb2e060a8b90ab21937dae
Dylan <dylan.reeder5@gmail.com>
2022-03-26 17:26:09 -0400

    adjusted oauth to work properly with no types

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -3,28 +3,23 @@ import GithubProvider from 'next-auth/providers/github'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import clientPromise from 'lib/mongodb'
 
-export default async function auth(req, res) {
-  const connection = await client.connect()
-  connection.db('test')
+export default NextAuth({
   // Configure one or more authentication providers
-  return NextAuth(req, res, {
-    providers: [
-      GithubProvider({
-        clientId: process.env.DEV_GITHUB_ID || process.env.GITHUB_ID,
-        clientSecret:
-          process.env.DEV_GITHUB_SECRET || process.env.GITHUB_SECRET,
-      }),
-      // ...add more providers here
-    ],
-    pages: {
-      signIn: '/signin',
+  providers: [
+    GithubProvider({
+      clientId: process.env.DEV_GITHUB_ID || process.env.GITHUB_ID,
+      clientSecret: process.env.DEV_GITHUB_SECRET || process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  pages: {
+    signIn: '/signin',
+  },
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log('info', token, user, account, profile, isNewUser)
+      return token
     },
-    callbacks: {
-      async jwt({ token, user, account, profile, isNewUser }) {
-        console.log('info', token, user, account, profile, isNewUser)
-        return token
-      },
-    },
-    adapter: MongoDBAdapter(clientPromise),
-  })
-}
+  },
+  adapter: MongoDBAdapter(clientPromise),
+})
