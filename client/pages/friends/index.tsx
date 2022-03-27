f3a8b8b6cd7b7091fe90d8e1b835f7b09a624d61
Overdrive141 <farhan.hammad@gmail.com>
2022-03-27 01:43:33 -0400

    fix token bug

--- client/pages/friends/index.tsx
+++ client/pages/friends/index.tsx
@@ -1,52 +1,52 @@
-import { ActivityCard } from '@components/ActivityCard';
+import { ActivityCard } from '@components/ActivityCard'
 import React from 'react'
-import { Box, Container } from '@mantine/core';
-import SearchBar from '@components/FriendsPage/SearchBar';
-
+import { Box, Container, Group } from '@mantine/core'
+import SearchBar from '@components/FriendsPage/SearchBar'
+import Navbar from '@components/Navbar'
 
 const index = () => {
-    
+  const activityLog = [
+    {
+      name: 'Jon Doe',
+      date: 'NO RECORDED ACTIVITY',
+      value: 24.31,
+    },
+    {
+      name: 'Jon Doe',
+      date: 'NO RECORDED ACTIVITY',
+      value: 24.31,
+    },
+    {
+      name: 'Jon Doe',
+      date: 'NO RECORDED ACTIVITY',
+      value: 24.31,
+    },
+    {
+      name: 'Jon Doe',
+      date: 'NO RECORDED ACTIVITY',
+      value: 24.31,
+    },
+  ]
 
-    const activityLog = [
-        {
-            name: 'Jon Doe',
-            date: 'NO RECORDED ACTIVITY',
-            value: 24.31,
-        },
-        {
-            name: 'Jon Doe',
-            date: 'NO RECORDED ACTIVITY',
-            value: 24.31,
-        },
-        {
-            name: 'Jon Doe',
-            date: 'NO RECORDED ACTIVITY',
-            value: 24.31,
-        }, {
-            name: 'Jon Doe',
-            date: 'NO RECORDED ACTIVITY',
-            value: 24.31,
-        }
+  // const renderActCard = () => (
 
-    ]
+  // )
 
-    // const renderActCard = () => (
+  const fields = activityLog.map((activity, index) => (
+    <ActivityCard key={index} activity={activity} />
+  ))
+  return (
+    <>
+      <Navbar />
+      <Container px="xs">
+        <Group>
+          <SearchBar />
+        </Group>
 
-    // )
-
-    const fields = activityLog.map((activity, index) => (
-        <ActivityCard key={index} activity={activity} />
-    ))
-    return (
-        <>
-        <Container>
-
-            <SearchBar />
-            <Box mx="auto">{fields}</Box>
-            </Container>
-        </>
-    )
+        <Box mx="auto">{fields}</Box>
+      </Container>
+    </>
+  )
 }
-
 
 export default index
