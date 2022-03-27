582873980c311e1888f09eea88a08e36e3b809b7
Dylan <dylan.reeder5@gmail.com>
2022-03-27 02:03:10 -0400

    WIP

--- client/components/modals/CreateSplitModal.tsx
+++ client/components/modals/CreateSplitModal.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7bd1891 (WIP: mock api and create split modal)
 import {
   Box,
   Button,
@@ -12,7 +8,6 @@ import {
   NativeSelect,
   NumberInput,
 } from '@mantine/core'
-<<<<<<< HEAD
 import React from 'react'
 import { z } from 'zod'
 import { useForm, zodResolver } from '@mantine/form'
@@ -58,7 +53,14 @@ export function CreateSplitModal({ opened, setOpened }: Props) {
         <form
           onSubmit={form.onSubmit((values) =>
             api
-              .post('/split', values)
+              .post(
+                '/split/',
+                {
+                  ...values,
+                  creator: session?.user?.id,
+                },
+                {}
+              )
               .then((res) => console.log(res))
               .catch((err) => console.error(err))
           )}
@@ -146,145 +148,5 @@ export function CreateSplitModal({ opened, setOpened }: Props) {
     </Modal>
   )
 }
-=======
-=======
->>>>>>> 7bd1891 (WIP: mock api and create split modal)
-import React from 'react'
-import { z } from 'zod'
-import { useForm, zodResolver } from '@mantine/form'
-import { CurrencyDollar } from 'tabler-icons-react'
-import api from '@/lib/axiosStore'
-import { useSession } from 'next-auth/react'
-
-type Props = {
-  opened: boolean
-  setOpened: (opened: boolean) => void
-}
-const schema = z.object({
-  name: z.string(),
-  amount: z.number().positive({ message: 'Invalid price' }),
-  category: z.string(),
-  people: z.array(z.string()).or(z.string()),
-})
-export function CreateSplitModal({ opened, setOpened }: Props) {
-  const { data: session } = useSession()
-  console.log(session)
-  const { colors } = useMantineTheme()
-  const form = useForm({
-    schema: zodResolver(schema),
-    initialValues: {
-      name: '',
-      people: '',
-      amount: '',
-      category: '',
-    },
-  })
-  return (
-    <Modal
-      opened={opened}
-      onClose={() => setOpened(false)}
-      title="Create Split"
-      centered
-      size={'xl'}
-      padding={52}
-      styles={{ modal: { border: `2px solid ${colors.dark[8]}` } }}
-      radius={16}
-    >
-      <Box>
-        <form
-          onSubmit={form.onSubmit((values) =>
-            api
-              .post('splits', values)
-              .then((res) => console.log(res))
-              .catch((err) => console.error(err))
-          )}
-        >
-          <TextInput
-            size="sm"
-            styles={{
-              input: {
-                backgroundColor: colors.dark[8],
-                '&:focus': {
-                  outline: 'none',
-                  border: 'transparent',
-                },
-              },
-            }}
-            required
-            error={form.errors.name}
-            label="Name"
-            placeholder="Night Out 16th Jan"
-            {...form.getInputProps('name')}
-          />
-          <TextInput
-            size="sm"
-            styles={{
-              input: {
-                backgroundColor: colors.dark[8],
-                '&:focus': {
-                  outline: 'none',
-                  border: 'transparent',
-                },
-              },
-            }}
-            required
-            error={form.errors.people}
-            label="Friends"
-            placeholder="John Doe, Jane Doe"
-            {...form.getInputProps('people')}
-          />
-          <NativeSelect
-            data={['Transportation', 'Food', 'Bills']}
-            size="sm"
-            styles={{
-              input: {
-                backgroundColor: colors.dark[8],
-                '&:focus': {
-                  outline: 'none',
-                  border: 'transparent',
-                },
-              },
-            }}
-            required
-            error={form.errors.category}
-            label="Category"
-            placeholder="Pick a category"
-            mt="sm"
-            {...form.getInputProps('category')}
-          />
-          <NumberInput
-            icon={<CurrencyDollar size={18} />}
-            size="sm"
-            styles={{
-              input: {
-                backgroundColor: colors.dark[8],
-                '&:focus': {
-                  outline: 'none',
-                  border: 'transparent',
-                },
-              },
-            }}
-            required
-            error={form.errors.amount}
-            label="Name"
-            placeholder="25.32"
-            mt="sm"
-            {...form.getInputProps('amount')}
-          />
-
-          <Group position="right" mt="xl">
-            <Button type="submit" sx={{ background: colors.primary[5] }}>
-              Submit
-            </Button>
-          </Group>
-        </form>
-      </Box>
-    </Modal>
-  )
-}
-<<<<<<< HEAD
 
 export default CreateSplitModal
->>>>>>> 14963bb (WIP: started with create split modal)
-=======
->>>>>>> 7bd1891 (WIP: mock api and create split modal)
