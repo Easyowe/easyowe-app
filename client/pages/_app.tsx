30bd2f8a7c297b3d900bd5a9c760de7021c1365c
Overdrive141 <farhan.hammad@gmail.com>
2022-03-26 21:36:17 -0400

    Yarn

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
-import {   ColorScheme,
+import {
+  ColorScheme,
   ColorSchemeProvider,
-  MantineProvider, } from '@mantine/core'
+  MantineProvider,
+} from '@mantine/core'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 
@@ -13,48 +15,48 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // const dark = colorScheme === 'dark';
 
   return (
-        <SessionProvider session={session}>
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{
-          // https://mantine.dev/theming/extend-theme/#extend-or-replace-colors
-          colorScheme,
-          colors: {
-            dark: [
-              '#F2F2F3',
-              '#E5E5E6',
-              '#CACACE',
-              '#B0B0B5',
-              '#95959D',
-              '#1A1A1C',
-              '#4A4A4F',
-              '#1A1A1C',
-              '#313135',
-              '#19191A',
-            ],
-            primary: [
-              '#EBE9FC',
-              '#D8D3F8',
-              '#B1A7F1',
-              '#897BEA',
-              '#624EE4',
-              '#5E4AE3',
-              '#2F1BB1',
-              '#1B0A8E',
-              '#0A096D',
-              '#0A096D',
-            ],
-          },
-          primaryColor: 'primary',
-        }}
-        withGlobalStyles
+    <SessionProvider session={session}>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          theme={{
+            // https://mantine.dev/theming/extend-theme/#extend-or-replace-colors
+            colorScheme,
+            colors: {
+              dark: [
+                '#F2F2F3',
+                '#E5E5E6',
+                '#CACACE',
+                '#B0B0B5',
+                '#95959D',
+                '#1A1A1C',
+                '#4A4A4F',
+                '#1A1A1C',
+                '#313135',
+                '#19191A',
+              ],
+              primary: [
+                '#EBE9FC',
+                '#D8D3F8',
+                '#B1A7F1',
+                '#897BEA',
+                '#624EE4',
+                '#5E4AE3',
+                '#2F1BB1',
+                '#1B0A8E',
+                '#0A096D',
+                '#0A096D',
+              ],
+            },
+            primaryColor: 'primary',
+          }}
+          withGlobalStyles
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </SessionProvider>
   )
 }
