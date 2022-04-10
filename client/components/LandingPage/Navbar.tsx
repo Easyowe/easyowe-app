5984f2b56d76e043ef5c9afa1b65e57c8ec4b1bc
Dylan <dylan.reeder5@gmail.com>
2022-04-10 17:19:58 -0400

    base responsive navbar

--- client/components/LandingPage/Navbar.tsx
+++ client/components/LandingPage/Navbar.tsx
@@ -1,74 +1,135 @@
-import { Box, Button, Group, Text, useMantineTheme } from '@mantine/core'
+import {
+  Box,
+  Button,
+  Group,
+  Text,
+  useMantineTheme,
+  Burger,
+  Center,
+} from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Navbar = () => {
-  const { colors } = useMantineTheme()
+type NavbarProps = {
+  open: boolean
+}
+
+const MobileMenu = ({ open }: NavbarProps) => {
   return (
     <Group
-      sx={(theme) => ({
-        borderBottom: `1px solid ${theme.colors.dark[8]}`,
-        padding: '1em 2em',
-        width: '100%',
-        position: 'fixed',
+      sx={{
+        position: 'absolute',
+        backgroundImage: `linear-gradient(to top right, rgba(26, 26, 28, 0.75), #3532367e)`,
+        backdropFilter: 'blur(4px)',
         top: 0,
         right: 0,
-        zIndex: 10,
-        backgroundImage: `linear-gradient(to top right, rgba(26, 26, 28, 0.50), #3532367e)`,
-        backdropFilter: 'blur(4px)',
-      })}
-      position="apart"
+        width: '100%',
+        height: '100%',
+        transform: open ? 'translateX(0%)' : 'translateX(100%)',
+        transition: 'all 500ms ease',
+        zIndex: 20,
+      }}
     >
-      <Box sx={{ position: 'relative', width: '4em', height: '4em' }}>
-        <Image layout="fill" src={'/logo-transparent.svg'} alt="easyowe logo" />
-      </Box>
-      <Group spacing={72}>
-        <Group>
+      <Center sx={{ width: '100%' }}>
+        <Group direction="column">
           <Link href="/">
-            <a>
-              <Text size="md" weight={500} color={colors.dark[1]}>
-                Home
-              </Text>
-            </a>
+            <a style={{ fontSize: '1.75em', fontWeight: 'bold' }}>Home</a>
           </Link>
-          <Link href="/">
-            <a>
-              <Text size="md" weight={500} color={colors.dark[1]}>
-                About
-              </Text>
-            </a>
+          <Link href="/about">
+            <a style={{ fontSize: '1.75em', fontWeight: 'bold' }}>About</a>
           </Link>
         </Group>
-        <Group>
-          <Button
-            size="md"
-            onClick={() => signIn()}
-            sx={(theme) => ({
-              backgroundColor: theme.colors.primary[5],
-              width: 'fit-content',
-              '&:hover': {
-                backgroundColor: theme.colors.primary[4],
-              },
-            })}
-          >
-            Sign Up
-          </Button>
-          <Button
-            size="md"
-            variant="subtle"
-            onClick={() => signIn()}
-            sx={(theme) => ({
-              width: 'fit-content',
-              color: theme.colors.dark[1],
-            })}
-          >
-            Log In
-          </Button>
+      </Center>
+    </Group>
+  )
+}
+
+const Navbar = () => {
+  const { colors } = useMantineTheme()
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState<boolean>(false)
+
+  const openMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen)
+
+  const isSmall = useMediaQuery('(max-width: 676px)')
+  return (
+    <>
+      <MobileMenu open={mobileMenuOpen} />
+      <Group
+        sx={(theme) => ({
+          borderBottom: `1px solid ${theme.colors.dark[8]}`,
+          padding: '1em 2em',
+          width: '100%',
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          zIndex: 30,
+          backgroundImage: `linear-gradient(to top right, rgba(26, 26, 28, 0.50), #3532367e)`,
+          backdropFilter: 'blur(4px)',
+        })}
+        position="apart"
+      >
+        <Box sx={{ position: 'relative', width: '4em', height: '4em' }}>
+          <Image
+            layout="fill"
+            src={'/logo-transparent.svg'}
+            alt="easyowe logo"
+          />
+        </Box>
+        <Group spacing={72} sx={{ zIndex: 30 }}>
+          {isSmall ? (
+            <Burger opened={mobileMenuOpen} onClick={openMobileMenu} />
+          ) : (
+            <>
+              <Group>
+                <Link href="/">
+                  <a>
+                    <Text size="md" weight={500} color={colors.dark[1]}>
+                      Home
+                    </Text>
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a>
+                    <Text size="md" weight={500} color={colors.dark[1]}>
+                      About
+                    </Text>
+                  </a>
+                </Link>
+              </Group>
+              <Group>
+                <Button
+                  size="md"
+                  onClick={() => signIn()}
+                  sx={(theme) => ({
+                    backgroundColor: theme.colors.primary[5],
+                    width: 'fit-content',
+                    '&:hover': {
+                      backgroundColor: theme.colors.primary[4],
+                    },
+                  })}
+                >
+                  Sign Up
+                </Button>
+                <Button
+                  size="md"
+                  variant="subtle"
+                  onClick={() => signIn()}
+                  sx={(theme) => ({
+                    width: 'fit-content',
+                    color: theme.colors.dark[1],
+                  })}
+                >
+                  Log In
+                </Button>
+              </Group>
+            </>
+          )}
         </Group>
       </Group>
-    </Group>
+    </>
   )
 }
 
