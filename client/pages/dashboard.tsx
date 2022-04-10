c380139fa4de7598e38a535917e388bba07d2894
Dylan <dylan.reeder5@gmail.com>
2022-04-10 16:00:51 -0400

    added simple coming soon page to dashboard

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -3,23 +3,44 @@ import Forms from '@components/Forms'
 import Navbar from '@components/Navbar'
 import Overview from '@components/Overview'
 import ThemeButton from '@components/ThemeButton'
-import { Container } from '@mantine/core'
+import { Center, Container, Group, Title, useMantineTheme } from '@mantine/core'
 import { NextPage } from 'next'
-import { useSession } from 'next-auth/react'
 import React from 'react'
+import { Rocket } from 'tabler-icons-react'
 
 const Dashboard: NextPage = () => {
-  const { data: session } = useSession()
-  console.log(session)
+  const { colors } = useMantineTheme()
   return (
     <>
       <Navbar />
-      <Container size="xl" py={52}>
-        <Overview />
-        <Forms />
-        <ActivityLog />
-        <ThemeButton />
-      </Container>
+      {process.env.NEXT_PUBLIC_ENVIRONMENT !== 'development' ? (
+        <Container size="xl" py={52}>
+          <Overview />
+          <Forms />
+          <ActivityLog />
+          <ThemeButton />
+        </Container>
+      ) : (
+        <Container size="xl" py={52}>
+          <Center sx={{ height: '75vh' }}>
+            <Group direction="column" align={'center'}>
+              <Title
+                order={1}
+                sx={{
+                  fontSize: '3em',
+                  margin: '-1em',
+                  zIndex: '10',
+                  textTransform: 'uppercase',
+                  color: colors.dark[2],
+                }}
+              >
+                Coming Soon
+              </Title>
+              <Rocket size={102} color={colors.dark[6]} />
+            </Group>
+          </Center>
+        </Container>
+      )}
     </>
   )
 }
